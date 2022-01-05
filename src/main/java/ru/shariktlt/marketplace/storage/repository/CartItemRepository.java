d3f6e397b2575dcb4f646adf78ab1d61f973ee16
Constantin Sharypov <shariktlt@gmail.com>
2022-01-05 22:50:59 +0300

    правка репозитория

--- src/main/java/ru/shariktlt/marketplace/storage/repository/CartItemRepository.java
+++ src/main/java/ru/shariktlt/marketplace/storage/repository/CartItemRepository.java
@@ -1,14 +1,16 @@
 package ru.shariktlt.marketplace.storage.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.shariktlt.marketplace.model.CartItem;
 
 public interface CartItemRepository extends CrudRepository<CartItem, CartItem.CartItemId> {
 
-    @Query("SELECT c FROM CartItem c WHERE id.id = :id")
-    Iterable<CartItem> findById(String id);
-
+    /**
+     * Поиск по CartItem.id.id
+     *
+     * @param id
+     * @return
+     */
     Iterable<CartItem> findByIdId(String id);
 
     void deleteById(String id);
